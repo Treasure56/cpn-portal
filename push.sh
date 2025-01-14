1df32f09a006cf9823fdb987bd5566691b1441ae
treasure56 <simplicitytreasure@gmail.com>
2025-01-14 07:22:04 -0800

    changed manager student filter

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ git add .
 
 ECHO "________________Added________________"
 
-git commit -m " added: filter to the admin and manager payments"
+git commit -m " changed manager student filter"
 
 ECHO "________________Committed________________"
 
